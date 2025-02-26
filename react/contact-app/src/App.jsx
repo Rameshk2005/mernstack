e85c3d4cb726426a1ec2e002c6f59bd8ed807d9f
Rameshk2005 <rameshk182005@gmail.com>
2025-02-26 12:45:07 +0530

    Day 5 and 6

---
+++ react/contact-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './page/Home'
+
+const App = () => {
+    return (
+        <div>App
+    
+            <Home />
+        </div>
+    )
+}
+
+export default App
